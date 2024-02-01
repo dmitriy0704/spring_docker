8e453fc04f9b2b0f89aa6711b888d938e71a0214
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-02-01 21:01:49 +0300

    настройка конфигурации приложения и базы

---
+++ src/main/java/com/example/spring_docker/entity/Product.java
@@ -0,0 +1,27 @@
+package com.example.spring_docker.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
