68a63ca037df27e5b54eb75aaeecf39a435a6d50
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-02-01 22:17:22 +0300

    финально: работающая сборка spring-boot + postgres

--- src/main/java/com/example/spring_docker/SpringDockerApplication.java
+++ src/main/java/com/example/spring_docker/SpringDockerApplication.java
@@ -1,13 +1,31 @@
 package com.example.spring_docker;
 
+import com.example.spring_docker.entity.Product;
+import com.example.spring_docker.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class SpringDockerApplication {
+public class SpringDockerApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDockerApplication.class, args);
-	}
 
+    @Autowired
+    private ProductRepository repository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDockerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Product product = new Product(null, "name");
+        repository.save(product);
+
+        final List<Product> all = repository.findAll();
+        System.out.println(all);
+    }
 }
